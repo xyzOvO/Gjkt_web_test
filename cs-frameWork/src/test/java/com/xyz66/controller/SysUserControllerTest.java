423ac74e23d66665442f78bc72fbfdfbebf2b025
gjkt2001 <2910223554@qq.com>
2023-11-23 10:53:52 +0800

    断言(assert)的使用

--- cs-frameWork/src/test/java/com/xyz66/controller/SysUserControllerTest.java
+++ cs-frameWork/src/test/java/com/xyz66/controller/SysUserControllerTest.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSON;
 import com.xyz66.domain.ResponseResult;
 import com.xyz66.domain.entity.SysUser;
 import com.xyz66.service.SysUserService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -34,8 +35,11 @@ public class SysUserControllerTest {
         Serializable id = 1;
         SysUser sysUser = new SysUser();
         sysUser.setId(1L);
+        sysUser.setUserName("admain");
+        // 设置mock对象行为
         // 设置sysUserService的 getById 方法的返回值为 sysUser
         when(sysUserService.getById(id)).thenReturn(sysUser);
+        // 调用被测试的方法
         // 调用 sysUserController 的 selectOne 方法，并传入 id 参数
         ResponseResult<SysUser> responseResult = sysUserController.selectOne(id);
         // 如果 responseResult 的 Code 字段等于 200
@@ -43,5 +47,7 @@ public class SysUserControllerTest {
             // 打印 responseResult 的 JSON 字符串形式
             System.out.println(JSON.toJSONString(responseResult));
         }
+        // 断言验证结果
+        assert(responseResult.getData().getUserName().equals(sysUser.getUserName()));
     }
 }
