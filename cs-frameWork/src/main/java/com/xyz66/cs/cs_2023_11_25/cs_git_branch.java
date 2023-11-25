d0a500dfccf0f21ba83e9b1b5edcfec9cd1bd924
gjkt2001 <2910223554@qq.com>
2023-11-25 21:52:05 +0800

    分支测试

---
+++ cs-frameWork/src/main/java/com/xyz66/cs/cs_2023_11_25/cs_git_branch.java
@@ -0,0 +1,23 @@
+package com.xyz66.cs.cs_2023_11_25;
+
+/**
+ * @author xyz66 Email:2910223554@qq.com
+ * @since 2023/11/25 21:46
+ */
+public class cs_git_branch {
+    // git branch 分支测试
+    public static void main(String[] args) {
+        // 1.创建分支
+        // git branch 分支名
+        // git branch test
+        // git branch
+        // 2.切换分支
+        // git checkout 分支名
+        // git checkout test
+        // 3.创建并切换分支
+        // git checkout -b 分支名
+        // git checkout -b test
+        // 4.删除分支
+        // git branch -d 分支名
+    }
+}
