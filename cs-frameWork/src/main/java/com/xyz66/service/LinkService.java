2695abd7c5bd292ef74837b56ea8c255dc63975b
gjkt2001 <2910223554@qq.com>
2023-11-28 16:08:20 +0800

    Link友链表完成

---
+++ cs-frameWork/src/main/java/com/xyz66/service/LinkService.java
@@ -0,0 +1,15 @@
+package com.xyz66.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xyz66.domain.entity.Link;
+
+/**
+ * 友链(Link)表服务接口
+ *
+ * @author xyz66 Email:2910223554@qq.com
+ * @since 2023-11-28 15:53:55
+ */
+public interface LinkService extends IService<Link> {
+
+}
+
