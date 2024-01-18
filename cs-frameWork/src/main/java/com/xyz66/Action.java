cd2e6c5a71c9b9fad64961a533673c37453f07aa
gjkt <2910223554@qq.com>
2024-01-18 19:16:46 +0800

    测试依赖注入过程-SpringBoot上下文启动执行

--- cs-frameWork/src/main/java/com/xyz66/Action.java
+++ cs-frameWork/src/main/java/com/xyz66/Action.java
@@ -1,11 +1,7 @@
 package com.xyz66;
 
-import com.xyz66.web.service.TestService;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.Resource;
 
 /**
  * @author Gjkt
@@ -14,12 +10,7 @@ import javax.annotation.Resource;
  */
 @SpringBootApplication
 public class Action {
-    @Resource
-//    @Autowired
-    private TestService testService;// 手动装配要去扫描
-    @Test
-    public void main() {
-        // 测试注入->去12/18-Cs.java
+    public static void main(String[] args) {
         SpringApplication.run(Action.class);
     }
 }
